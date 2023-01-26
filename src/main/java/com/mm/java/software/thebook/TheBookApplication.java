ef52177d6fbce9c5a6ad2f51c6600f7322b26de7
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2023-01-27 00:41:18 +0100

    2/2 part of structure project

---
+++ src/main/java/com/mm/java/software/thebook/TheBookApplication.java
@@ -0,0 +1,13 @@
+package com.mm.java.software.thebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBookApplication.class, args);
+	}
+
+}
